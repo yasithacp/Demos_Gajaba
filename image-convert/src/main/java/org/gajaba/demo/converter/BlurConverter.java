f5ae0587993ef12e3f228eba296ae809b88b675e
Yasitha Pandithawatta <yasitha4@gmail.com>
2012-10-18 13:16:08 +0530

    Implement Image blur functionality

---
+++ image-convert/src/main/java/org/gajaba/demo/converter/BlurConverter.java
@@ -0,0 +1,24 @@
+package org.gajaba.demo.converter;
+
+
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
+
+public class BlurConverter implements ImageConverter {
+    @Override
+    public BufferedImage convert(BufferedImage img) {
+
+        float[] floats = new float[81];
+        for (int i = 0; i < floats.length; i++) {
+            floats[i] = 1f/81f;
+        }
+
+        Kernel kernel = new Kernel(9, 9,floats);
+        BufferedImageOp op = new ConvolveOp(kernel, ConvolveOp.EDGE_NO_OP, null);
+        BufferedImage result = op.filter(img, null);
+        return result;
+    }
+
+}
